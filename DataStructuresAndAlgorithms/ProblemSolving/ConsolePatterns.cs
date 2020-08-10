e6c8465b1585bea71d06bfc7139461e4a5eaa465
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-10 23:19:52 +0200

    Add ConsolesPatterns

---
+++ DataStructuresAndAlgorithms/ProblemSolving/ConsolePatterns.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms.ProblemSolving
+{
+    class ConsolePatterns
+    {
+        public void UpsideDownNumberPyramid(int k)
+        {
+            for (int i = 1; i <= k; i++)
+            {
+                for (int j = k; j >= 1; j--)
+                {
+                    if (j >= i)
+                        Console.Write(j);
+                    else
+                        Console.Write(' ');
+                }
+                for (int j = 2; j <= k; j++)
+                {
+                    if (j >= i)
+                        Console.Write(j);
+                    else
+                        Console.Write(' ');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
