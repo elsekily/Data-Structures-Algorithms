be780f1d3ce1f2d16947fadf074ea837dc7f3960
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-11 00:03:12 +0200

    Add Brackets Balance Check and main comments

--- DataStructuresAndAlgorithms/LeetCode/MaximumProductofSplittedBinaryTree.cs
+++ DataStructuresAndAlgorithms/LeetCode/MaximumProductofSplittedBinaryTree.cs
@@ -49,3 +49,15 @@ namespace DataStructuresAndAlgorithms.LeetCode
 
     }
 }
+/*
+ var root1 = new TreeNode(1,
+                new TreeNode(2
+                , new TreeNode(4), new TreeNode(5)), new TreeNode(3, new TreeNode(6)));
+
+            var root2 = new TreeNode(1, null,
+                new TreeNode(2, new TreeNode(3)
+                , new TreeNode(4, new TreeNode(5), new TreeNode(6))));
+            var x = new MaximumProductofSplittedBinaryTree();
+            System.Console.WriteLine(x.MaxProduct(root1));
+            System.Console.WriteLine(x.MaxProduct(root2));
+*/
---
+++ DataStructuresAndAlgorithms/ProblemSolving/BracketsBalanceCheck.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms.ProblemSolving
+{
+    public class BracketsBalanceCheck
+    {
+        private List<char> leftBracket;
+        private List<char> rightBracket;
+        public BracketsBalanceCheck()
+        {
+            leftBracket = new List<char>()
+            {
+                '(', '<', '{', '['
+            };
+            rightBracket = new List<char>()
+            {
+                ')', '>', '}', ']'
+            };
+        }
+        public bool CheckBalance(string input)
+        {
+            //60,62,91,93,40,41
+            if (input == null)
+                throw new Exception();
+
+            var stack = new Stack<char>();
+            foreach (var item in input)
+            {
+                if (leftBracket.Contains(item))
+                {
+                    stack.Push(item);
+                }
+                if (rightBracket.Contains(item))
+                {
+                    if (stack.Count == 0)
+                        return false;
+                    if (leftBracket.IndexOf(stack.Peek()) != rightBracket.IndexOf(item))
+                        return false;
+                    stack.Pop();
+                }
+            }
+            if (stack.Count > 0)
+                return false;
+            return true;
+        }
+    }
+}
