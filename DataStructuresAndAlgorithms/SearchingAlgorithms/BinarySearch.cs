f9c17c0f2ff8015e73fb9f387f5380896d42d9a2
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-10 04:24:38 +0200

    Adding Searching and BinarySearch

---
+++ DataStructuresAndAlgorithms/SearchingAlgorithms/BinarySearch.cs
@@ -0,0 +1,25 @@
+﻿namespace DataStructuresAndAlgorithms.SearchingAlgorithms
+{
+    public class BinarySearch
+    {
+        public bool SearchByRecurrsion(int[] Array, int number)
+        {
+            return SearchByRecurrsion(Array, 0, Array.Length - 1, number);
+        }
+        private bool SearchByRecurrsion(int[] Array, int start, int end, int number)
+        {
+            if (start > end)
+                return false;
+            int mid = (start + end) / 2;
+
+            if (Array[mid] == number)
+                return true;
+            if (Array[mid] < number)
+                return SearchByRecurrsion(Array, mid + 1, end, number);
+            if (Array[mid] > number)
+                return SearchByRecurrsion(Array, start, mid - 1, number);
+
+            return false;
+        }
+    }
+}
