5e5b037ef59aa81f45a5660f1a5529428749def7
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-14 07:01:58 +0200

    Add String to Integer

---
+++ DataStructuresAndAlgorithms/LeetCode/StringtoInteger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class StringtoInteger
+    {
+        public int MyAtoi(string str)
+        {
+            var myStr = str.TrimStart();
+            
+            if (myStr.Length == 0)
+                return 0;
+
+            var num = 0;
+            var digits = 0;
+            var sign = 1;
+            
+            if (!IsDigitOrSign(myStr[0]))
+                return 0;
+
+            foreach (var digit in myStr)
+            {
+                if ((digits == 0) && (digit == '-'))
+                    sign = -1;
+
+                if (!(digit >= 48 && digit < 58) && digits != 0)
+                    break;
+
+                checked
+                {
+                    try
+                    {
+                        if (digit >= 48 && digit < 58)
+                            num = (num * 10) + (digit - 48);
+                    }
+                    catch
+                    {
+                        if(sign == 1) return int.MaxValue;
+                        return int.MinValue;
+                    }
+                }
+                digits++;
+            }
+            return num * sign;
+        }
+        private bool IsDigitOrSign(char x)
+        {
+            if ((x >= 48 && x < 58) || x == '+' || x == '-')
+                return true;
+            return false;
+        }
+    }
+}
--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -1,26 +1,10 @@
-﻿using DataStructuresAndAlgorithms.MITProgramming_for_the_Puzzled;
-
-namespace DataStructuresAndAlgorithms
+﻿namespace DataStructuresAndAlgorithms
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var x = new Puzzle1YouWillAllConform();
-            x.PleaseConfirm(new char[]
-            {
-                'F',
-                'B',
-                'B',
-                'F',
-                'B',
-                'F',
-                'F',
-                'B',
-                'B',
-                'B',
-                'F'
-            });
+            
         }
     }
 }
