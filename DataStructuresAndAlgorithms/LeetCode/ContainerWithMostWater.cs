38183cf8050b77be00a2775c955a9bc809d55e1a
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-16 20:31:02 +0200

    solving Container With Most Water

---
+++ DataStructuresAndAlgorithms/LeetCode/ContainerWithMostWater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            int maxArea = 0;
+            for (int i = 0; i < height.Length - 1; i++)
+            {
+                for (int j = i + 1; j < height.Length; j++) 
+                {
+                    var area = Math.Min(height[i], height[j]) * (j - i);
+                    maxArea = area > maxArea ? area : maxArea;
+                }
+            }
+            return maxArea;
+        }
+    }
+}
