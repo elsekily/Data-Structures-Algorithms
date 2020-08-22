10c9a1bf6315828ef99afc0517723f6d988d17ba
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-22 17:08:29 +0200

    update Pascal's Triangle

--- DataStructuresAndAlgorithms/LeetCode/PascalsTriangle.cs
+++ DataStructuresAndAlgorithms/LeetCode/PascalsTriangle.cs
@@ -7,25 +7,14 @@ namespace DataStructuresAndAlgorithms.LeetCode
         public IList<IList<int>> Generate(int numRows)
         {
             var list = new List<IList<int>>();
-            if (numRows == 0)
-                return list;
-            
-            list.Add(new List<int>() { 1 });
-            if (numRows == 1)
-                return list;
-            
-            list.Add(new List<int>() { 1, 1 });
-            if (numRows == 2)
-                return list;
 
-            for (int i = 2; i < numRows; i++)
+            for (int i = 0; i < numRows; i++)
             {
                 var subList = new List<int>();
                 for (int j = 0; j <= i; j++)
                 {
-                    if (j == 0) subList.Add(1);
-                    if (j == i) subList.Add(1);
-                    if (j != 0 && j != i) subList.Add(list[i - 1][j] + list[i - 1][j - 1]);
+                    if (j == 0 || j == i) subList.Add(1);
+                    else subList.Add(list[i - 1][j] + list[i - 1][j - 1]);
                 }
                 list.Add(subList);
             }
