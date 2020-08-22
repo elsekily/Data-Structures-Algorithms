a54c33e30f8072762ab1184801d25958fd1ee59e
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-22 16:50:19 +0200

    add Pascal's Triangle

---
+++ DataStructuresAndAlgorithms/LeetCode/PascalsTriangle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class PascalsTriangle
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            var list = new List<IList<int>>();
+            if (numRows == 0)
+                return list;
+            
+            list.Add(new List<int>() { 1 });
+            if (numRows == 1)
+                return list;
+            
+            list.Add(new List<int>() { 1, 1 });
+            if (numRows == 2)
+                return list;
+
+            for (int i = 2; i < numRows; i++)
+            {
+                var subList = new List<int>();
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == 0) subList.Add(1);
+                    if (j == i) subList.Add(1);
+                    if (j != 0 && j != i) subList.Add(list[i - 1][j] + list[i - 1][j - 1]);
+                }
+                list.Add(subList);
+            }
+            return list;
+        }
+    }
+}
