69e644a69a31f790f88d72df07914a90778356f2
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-27 01:46:33 +0200

    Adding Climbing Stairs

---
+++ DataStructuresAndAlgorithms/LeetCode/ClimbingStairs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    class ClimbingStairs
+    {
+        public int ClimbStairs(int n)
+        {
+            return ClimbRecursion(0, n);
+        }
+        private int ClimbRecursion(int i, int n)
+        {
+            if (i > n)
+                return 0;
+            if (i == n)
+                return 1;
+
+            return ClimbRecursion(i + 1, n) + ClimbRecursion(i + 2, n);
+        }
+    }
+}
