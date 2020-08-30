65a7c5a0f4ad6ca289ed660794e5ca29a4222ea2
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-30 13:53:48 +0200

    Adding Dynamic programming

--- DataStructuresAndAlgorithms/LeetCode/ClimbingStairs.cs
+++ DataStructuresAndAlgorithms/LeetCode/ClimbingStairs.cs
@@ -6,18 +6,25 @@ namespace DataStructuresAndAlgorithms.LeetCode
 {
     class ClimbingStairs
     {
+        private Dictionary<int, int> memory;
         public int ClimbStairs(int n)
         {
+            memory = new Dictionary<int, int>();
             return ClimbRecursion(0, n);
         }
         private int ClimbRecursion(int i, int n)
         {
+            if (memory.ContainsKey(i))
+                return memory[i];
+
             if (i > n)
                 return 0;
             if (i == n)
                 return 1;
 
-            return ClimbRecursion(i + 1, n) + ClimbRecursion(i + 2, n);
+            var result = ClimbRecursion(i + 1, n) + ClimbRecursion(i + 2, n);
+            memory.Add(i, result);
+            return result;
         }
     }
 }
