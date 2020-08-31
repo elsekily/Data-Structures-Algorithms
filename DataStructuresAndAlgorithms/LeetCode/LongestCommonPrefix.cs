ec356640c489cc7333012c86051c3272faf6f8a0
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-31 22:31:17 +0200

    Adding Longest Common Prefix

---
+++ DataStructuresAndAlgorithms/LeetCode/LongestCommonPrefix.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    class LongestCommonPrefix
+    {
+        public string MyLongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0 || strs == null)
+                return "";
+
+            var output = new StringBuilder();
+            output.Append(strs[0]);
+            for (int i = 1; i < strs.Length && output.Length != 0; i++)
+            {
+                if(strs[i].Length < output.Length)
+                    output.Remove(strs[i].Length, output.Length - strs[i].Length);
+
+                for (int j = 0; j < output.Length; j++)
+                {
+                    if (output[j] != strs[i][j])
+                    {
+                        output.Remove(j, output.Length - j);
+                        break;
+                    }
+                }
+            }
+      
+            return output.ToString();
+        }
+    }
+}
