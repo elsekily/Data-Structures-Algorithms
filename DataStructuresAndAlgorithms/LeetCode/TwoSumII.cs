92e5a8f970bddcfeb6c30579c6bda3db00136a85
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-31 19:22:44 +0200

    Adding Two Sum II

---
+++ DataStructuresAndAlgorithms/LeetCode/TwoSumII.cs
@@ -0,0 +1,30 @@
+﻿namespace DataStructuresAndAlgorithms.LeetCode
+{
+    class TwoSumII
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            if (numbers == null || numbers.Length == 0)
+                return new int[] { 0 };
+
+            var pointer1 = 0;
+            var pointer2 = numbers.Length - 1;
+
+            while (pointer1 != pointer2)
+            {
+                var sum = numbers[pointer1] + numbers[pointer2];
+                
+                if (sum == target)
+                    return new int[] { pointer1 + 1, pointer2 + 1 };
+
+                if (sum > target)
+                    pointer2--;
+
+                if (sum < target)
+                    pointer1++;
+            }
+
+            return new int[] { 0 };
+        }
+    }
+}
