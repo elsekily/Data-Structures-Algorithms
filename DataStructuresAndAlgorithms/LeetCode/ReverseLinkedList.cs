4ef8858c42efa3f68fa0913fb2e860fa177f59ee
Youssef Elsekily <youssefelsekily@outlook.com>
2020-09-01 19:34:25 +0200

    Add Another Solution to Reverse LinkedList

--- DataStructuresAndAlgorithms/LeetCode/ReverseLinkedList.cs
+++ DataStructuresAndAlgorithms/LeetCode/ReverseLinkedList.cs
@@ -27,5 +27,17 @@
             }
             return list;
         }
+        public ListNode AnotherSolution(ListNode head)
+        {
+            ListNode prev = null;
+            while (head != null) 
+            {
+                var next = head.next;
+                head.next = prev;
+                prev = head;
+                head = next;
+            }
+            return prev;
+        }
     }
  }
