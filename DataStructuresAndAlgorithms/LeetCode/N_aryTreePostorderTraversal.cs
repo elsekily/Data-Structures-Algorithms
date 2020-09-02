5173b9c3bed34d770df2b554cfb6a9f65129ca7e
Youssef Elsekily <youssefelsekily@outlook.com>
2020-09-02 16:17:14 +0200

    Adding ReadMe and  N-ary Postorder Traversal

---
+++ DataStructuresAndAlgorithms/LeetCode/N_aryTreePostorderTraversal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+    public class N_aryTreePostorderTraversal
+    {
+        public IList<int> Postorder(Node root)
+        {
+            if (root == null)
+                return new List<int>(); 
+
+            var list = new List<int>();
+            Recursion(root, list);
+            return list;
+        }
+        private void Recursion(Node root, IList<int> list)
+        {
+            foreach (var child in root.children)
+            {
+                Recursion(child, list);
+            }
+            list.Add(root.val);
+        }
+    }
+ }
---
+++ README.md
@@ -0,0 +1,8 @@
+# Data Structures and Algorithms 
+I'm studying algorithms and data structures also solving problems from many sources. here I implement these problems using C# giving each has its namespace.
+
+    Dynamic Programming
+    Leet Code
+    MIT Courses
+    Code With Mosh - Data Structure and Algorithm
+    Many Other Sources
