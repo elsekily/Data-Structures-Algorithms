57d3f6805e623d6f6b5e9482890391fd2d283457
Youssef Elsekily <youssefelsekily@outlook.com>
2020-09-03 19:17:36 +0200

    Add linkedList Utilities

---
+++ DataStructuresAndAlgorithms/LinkedLists/Utilities.cs
@@ -0,0 +1,39 @@
+﻿namespace DataStructuresAndAlgorithms.LinkedLists
+{
+    public class Utilities<T>
+    {
+        public T GetMiddle(System.Collections.Generic.LinkedList<T> list)
+        {
+            var slow = list.First;
+            var fast = list.First;
+
+            while (fast != list.Last && fast.Next != list.Last)
+            {
+                slow = slow.Next;
+                fast = fast.Next.Next;
+            }
+
+            if (fast == list.Last)
+                return slow.Value;
+
+            return slow.Value;
+        }
+
+        public bool CheckLoop(System.Collections.Generic.LinkedList<T> list)
+        {
+            var slow = list.First;
+            var fast = list.First;
+
+            while (fast != list.Last && fast.Next != list.Last)
+            {
+                if (slow == fast)
+                    return true;
+
+                slow = slow.Next;
+                fast = fast.Next.Next;
+            }
+
+            return false;
+        }
+    }
+}
