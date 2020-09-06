a0d8e3cdf96e99c2776f05f0b3d47e3ef6bf4d86
Youssef Elsekily <youssefelsekily@outlook.com>
2020-09-06 20:20:33 +0200

    Adding Invert Binary Tree

---
+++ DataStructuresAndAlgorithms/LeetCode/InvertBinaryTree.cs
@@ -0,0 +1,21 @@
+﻿using DataStructuresAndAlgorithms.LeetCode.InputTypes;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class InvertBinaryTree
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+                return root;
+
+            var left = InvertTree(root.left);
+            var right = InvertTree(root.right);
+
+            root.left = right;
+            root.right = left;
+
+            return root;
+        }
+    }
+}
