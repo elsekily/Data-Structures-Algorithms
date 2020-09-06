e099fbc0d5bdbfb88b0f47d57e324e70d9d8b5a5
Youssef Elsekily <youssefelsekily@outlook.com>
2020-09-06 20:05:21 +0200

    Adding Merge Two Binary Trees

---
+++ DataStructuresAndAlgorithms/LeetCode/MergeTwoBinaryTrees.cs
@@ -0,0 +1,20 @@
+﻿using DataStructuresAndAlgorithms.LeetCode.InputTypes;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class MergeTwoBinaryTrees
+    {
+        public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null)
+                return t2;
+            if (t2 == null)
+                return t1;
+
+            t1.val += t2.val;
+            t1.left = MergeTrees(t1.left, t2.left);
+            t1.right = MergeTrees(t1.right, t2.right);
+            return t1;
+        }
+    }
+}
