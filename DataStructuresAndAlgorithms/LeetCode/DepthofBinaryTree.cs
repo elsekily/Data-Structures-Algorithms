cf01c4443caa2a5f4ae11bbc292f4252f0150853
Youssef Elsekily <youssefelsekily@outlook.com>
2020-09-06 23:29:59 +0200

    Adding Depth of Binary Tree

---
+++ DataStructuresAndAlgorithms/LeetCode/DepthofBinaryTree.cs
@@ -0,0 +1,31 @@
+﻿using DataStructuresAndAlgorithms.LeetCode.InputTypes;
+using System;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class DepthofBinaryTree
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            
+            if (root == null)
+                return 0;
+            return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+        }
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            var right = MinDepth(root.right);
+            var left = MinDepth(root.left);
+
+            if (left != 0 && right == 0)
+                return 1 + left;
+
+            if (left == 0 && right != 0)
+                return 1 + right;
+
+            return 1 + Math.Max(left, right);
+        }
+    }
+}
