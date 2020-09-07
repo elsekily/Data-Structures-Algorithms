abf994b66d575e8b4c958d52811648e510a2fc2e
Youssef Elsekily <youssefelsekily@outlook.com>
2020-09-07 23:28:46 +0200

    Adding PathSum and SymmetricTree

---
+++ DataStructuresAndAlgorithms/LeetCode/PathSum.cs
@@ -0,0 +1,30 @@
+﻿using DataStructuresAndAlgorithms.LeetCode.InputTypes;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class PathSum
+    {
+        private int HasSum(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            return root.val + HasSum(root.right);
+        }
+        public bool HasPathSum(TreeNode root, int sum)
+        {
+            return Sum(root, sum, 0);
+        }
+        private bool Sum(TreeNode root, int sum, int currentSum)
+        {
+            if (root == null) 
+                return false;
+
+            currentSum += root.val;
+            if (root.right == null && root.left == null && currentSum == sum)
+                return true;
+
+            return Sum(root.right, sum, currentSum) || Sum(root.left, sum, currentSum);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/LeetCode/SymmetricTree.cs
@@ -0,0 +1,24 @@
+﻿using DataStructuresAndAlgorithms.LeetCode.InputTypes;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class SymmetricTree
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null)
+                return true;
+
+            return MirrorCheck(root.left, root.right);
+        }
+        private bool MirrorCheck(TreeNode t1,TreeNode t2)
+        {
+            if (t1 == null && t2 == null)
+                return true;
+            if (t1 == null || t2 == null)
+                return false;
+
+            return t1.val == t2.val && MirrorCheck(t1.left, t2.right) && MirrorCheck(t1.right, t2.left);
+        }
+    }
+}
