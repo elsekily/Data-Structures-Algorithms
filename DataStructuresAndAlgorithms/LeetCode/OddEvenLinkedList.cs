e80a4855e71c80c3c2867a30fc23ac25fb93c219
Youssef Elsekily <youssefelsekily@outlook.com>
2020-09-08 17:07:26 +0200

    Adding Odd Even LinkedList

---
+++ DataStructuresAndAlgorithms/LeetCode/OddEvenLinkedList.cs
@@ -0,0 +1,24 @@
+﻿using DataStructuresAndAlgorithms.LeetCode.InputTypes;
+using System.Runtime.InteropServices;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class OddEvenLinkedList
+    {
+        public ListNode OddEvenList(ListNode head)
+        {
+            var odd = head;
+            var evenHead = head.next;
+            var even = evenHead;
+            while (even != null && even.next != null)
+            {
+                odd.next = even.next;
+                odd = odd.next;
+                even.next = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead;
+            return head;
+        }
+    }
+}
