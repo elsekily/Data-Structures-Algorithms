229b975611b65b7c64103e45ee40d5238e7c5787
Youssef Elsekily <youssefelsekily@outlook.com>
2020-09-10 19:17:40 +0200

    Adding Merge Two Sorted Lists

---
+++ DataStructuresAndAlgorithms/LeetCode/MergeTwoSortedLists.cs
@@ -0,0 +1,40 @@
+﻿using DataStructuresAndAlgorithms.LeetCode.InputTypes;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class MergeTwoSortedLists
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null)
+                return l2;
+
+            if (l2 == null)
+                return l1;
+
+            ListNode head = new ListNode();
+            var output = head;
+
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val < l2.val)
+                {
+                    output.next = new ListNode(l1.val);
+                    l1 = l1.next;
+                }
+                else
+                {
+                    output.next = new ListNode(l2.val);
+                    l2 = l2.next;
+                }
+                output = output.next;
+            }
+            if (l1 != null)
+                output.next = l1;
+            if (l2 != null)
+                output.next = l2;
+
+            return head.next;
+        }
+    }
+}
