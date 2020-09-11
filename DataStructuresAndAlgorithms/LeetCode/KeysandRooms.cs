dcf2463b0e89ee906d716fb5aab167ab658314d6
Youssef Elsekily <youssefelsekily@outlook.com>
2020-09-11 19:11:05 +0200

    Adding Keys and Rooms

---
+++ DataStructuresAndAlgorithms/LeetCode/KeysandRooms.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class KeysandRooms
+    {
+        public bool CanVisitAllRooms(IList<IList<int>> rooms)
+        {
+            var set = new HashSet<int>();
+            var stack = new Stack<int>();
+            stack.Push(0);
+            while (stack.Count != 0)
+            {
+                var index = stack.Pop();
+                if(!set.Contains(index))
+                {
+                    set.Add(index);
+                    for (int i = 0; i < rooms[index].Count; i++)
+                    {
+                        stack.Push(rooms[index][i]);
+                    }
+                }
+            }
+            return set.Count == rooms.Count;
+        }
+    }
+}
