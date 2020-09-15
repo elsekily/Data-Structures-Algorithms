0aead47ab4b5315a24fd08d346193b77c855e98a
Youssef Elsekily <youssefelsekily@outlook.com>
2020-09-15 18:09:56 +0200

    Adding Find Largest Value in Each Tree Row

---
+++ DataStructuresAndAlgorithms/LeetCode/FindLargestValueinEachTreeRow.cs
@@ -0,0 +1,30 @@
+﻿using DataStructuresAndAlgorithms.LeetCode.InputTypes;
+using System;
+using System.Collections.Generic;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class FindLargestValueinEachTreeRow
+    {
+        private List<int> list;
+        public IList<int> LargestValues(TreeNode root)
+        {
+            list = new List<int>();
+            TreePreorderTraversal(root, 0);
+            return list;
+        }
+        private void TreePreorderTraversal(TreeNode root, int level)
+        {
+            if (root == null)
+                return;
+
+            if (list.Count == level)
+                list.Add(root.val);
+            else
+                list[level] = Math.Max(root.val, list[level]);
+
+            TreePreorderTraversal(root.left, level + 1);
+            TreePreorderTraversal(root.right, level + 1);
+        }
+    }
+}
