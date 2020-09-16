5d337ecbe1d5667a01cd4df02ac1b3fcd454fe54
Youssef Elsekily <youssefelsekily@outlook.com>
2020-09-16 19:09:53 +0200

    Adding Binary Tree Pruning

---
+++ DataStructuresAndAlgorithms/LeetCode/BinaryTreePruning.cs
@@ -0,0 +1,25 @@
+﻿using DataStructuresAndAlgorithms.LeetCode.InputTypes;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class BinaryTreePruning
+    {
+        public TreeNode PruneTree(TreeNode root)
+        {
+            return HasOne(root) ? null : root;
+        }
+        private bool HasOne(TreeNode root)
+        {
+            if (root == null) return false;
+
+            if (HasOne(root.left)) root.left = null;
+
+            if (HasOne(root.right)) root.right = null;
+
+            if (root.val == 0 && root.left == null && root.right == null)
+                return true;
+
+            return false;
+        }
+    }
+}
