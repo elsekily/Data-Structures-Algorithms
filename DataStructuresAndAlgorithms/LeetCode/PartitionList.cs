9398cacc92c8665768caa26d9a0772ba92f2e6dd
Youssef Elsekily <youssefelsekily@outlook.com>
2020-09-20 23:11:15 +0200

    Adding Partition List

---
+++ DataStructuresAndAlgorithms/LeetCode/PartitionList.cs
@@ -0,0 +1,35 @@
+﻿using DataStructuresAndAlgorithms.LeetCode.InputTypes;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class PartitionList
+    {
+        public ListNode Partition(ListNode head, int x)
+        {
+            var smaller = new ListNode();
+            var smallerPointer = smaller;
+            var greater = new ListNode();
+            var greaterPointer = greater;
+
+            while (head != null)
+            {
+                if (head.val < x)
+                {
+                    smallerPointer.next = head;
+                    smallerPointer = smallerPointer.next;
+                }
+                else
+                {
+                    greaterPointer.next = head;
+                    greaterPointer = greaterPointer.next;
+                }
+                head = head.next;
+            }
+
+            smallerPointer.next = greater.next;
+            greaterPointer.next = null;
+
+            return smaller.next;
+        }
+    }
+}
