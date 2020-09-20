ee54fbbb3897f742905c6be1344460ac8cb09885
Youssef Elsekily <youssefelsekily@outlook.com>
2020-09-20 21:59:33 +0200

    Adding Hand of Straights

--- DataStructuresAndAlgorithms/LeetCode/HandofStraights.cs
+++ DataStructuresAndAlgorithms/LeetCode/HandofStraights.cs
@@ -1,9 +1,35 @@
-﻿namespace DataStructuresAndAlgorithms.LeetCode
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DataStructuresAndAlgorithms.LeetCode
 {
     public class HandofStraights
     {
         public bool IsNStraightHand(int[] hand, int W)
         {
+            var dict = new Dictionary<int, int>();
+            foreach (var card in hand)
+            {
+                if (dict.ContainsKey(card))
+                    dict[card]++;
+                else
+                    dict.Add(card, 1);
+            }
+            while (dict.Count != 0)
+            {
+                int first = dict.First().Key;
+                for (int i = first; i < first + W; i++) 
+                {
+                    if (!dict.ContainsKey(i))
+                        return false;
+
+                    if (dict[i] == 1)
+                        dict.Remove(i);
+                    else
+                        dict[i]--;
+                }
+            }
+
             return true;
         }
     }
