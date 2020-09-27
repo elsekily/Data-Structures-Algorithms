0d62db0e3e1160b329aaaf37e13071126abdd7f6
Youssef Elsekily <youssefelsekily@outlook.com>
2020-09-27 22:44:58 +0200

    Adding Check Completeness of a Binary Tree

---
+++ DataStructuresAndAlgorithms/LeetCode/CheckCompletenessofaBinaryTree.cs
@@ -0,0 +1,30 @@
+﻿using DataStructuresAndAlgorithms.LeetCode.InputTypes;
+using System.Collections.Generic;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class CheckCompletenessofaBinaryTree
+    {
+        public bool IsCompleteTree(TreeNode root)
+        {
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            var flag = false;
+            while (queue.Count != 0)
+            {
+                var current = queue.Dequeue();
+
+                if (current == null && !flag)
+                    flag = true;
+
+                if (current != null)
+                {
+                    if (flag) return false;
+                    queue.Enqueue(current.left);
+                    queue.Enqueue(current.right);
+                }
+            }
+            return true;
+        }
+    }
+}
