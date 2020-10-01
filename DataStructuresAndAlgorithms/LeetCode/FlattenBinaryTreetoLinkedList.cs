c5bc3c764b3fb7da3570eaee76b8891411d7c5d5
Youssef Elsekily <youssefelsekily@outlook.com>
2020-10-01 23:52:39 +0200

    Adding Flatten Binary Tree to Linked List

---
+++ DataStructuresAndAlgorithms/LeetCode/FlattenBinaryTreetoLinkedList.cs
@@ -0,0 +1,27 @@
+﻿using DataStructuresAndAlgorithms.LeetCode.InputTypes;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class FlattenBinaryTreetoLinkedList
+    {
+        public void Flatten(TreeNode root)
+        {
+            if (root == null)
+                return;
+
+            Flatten(root.left);
+            Flatten(root.right);
+
+            if (root.left != null)
+            {
+                var temp = root.right;
+                root.right = root.left;
+                root.left = null;
+                var current = root.right;
+                while (current.right != null)
+                    current = current.right;
+                current.right = temp;
+            }
+        }
+    }
+}
