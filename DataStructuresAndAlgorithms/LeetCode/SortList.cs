2998023a47ac806e950e17836be391ffd0dcaaea
Youssef Elsekily <youssefelsekily@outlook.com>
2020-10-03 02:19:05 +0200

    Adding Sort List

---
+++ DataStructuresAndAlgorithms/LeetCode/SortList.cs
@@ -0,0 +1,34 @@
+﻿using DataStructuresAndAlgorithms.LeetCode.InputTypes;
+
+namespace DataStructuresAndAlgorithms.LeetCode
+{
+    public class SortList
+    {
+        public ListNode SortListSol(ListNode head)
+        {
+            if (head == null) return head;
+
+            var list = new ListNode(head.val);
+            head = head.next;
+
+            while (head != null)
+            {
+                var curr = list;
+                if (curr.val > head.val)
+                {
+                    list = new ListNode(head.val, list);
+                }
+                else
+                {
+                    while (curr.next != null && curr.next.val < head.val)
+                        curr = curr.next;
+
+                    var temp = curr.next;
+                    curr.next = new ListNode(head.val, temp);
+                }
+                head = head.next;
+            }
+            return list;
+        }
+    }
+}
